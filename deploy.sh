5e59f6f1b31c450cc52c222c7e565ecf2ef4b758
Ricksters2000 <1012rvelez@gmail.com>
2021-08-15 14:25:11 -0400

    deploy file will now work

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,15 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+# git init
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Ricksters2000/algo-visualizer.git main:gh-pages
+# git push -f git@github.com:Ricksters2000/algo-visualizer.git main:gh-pages
+git push -u git@github.com:Ricksters2000/algo-visualizer.git gh-pages
 
 cd -
